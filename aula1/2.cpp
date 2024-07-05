d5d57294102dcd8a9c57806ca66b8b326214872c
Guilherme Brito <guilhermecbbrito@gmail.com>
2024-07-05 14:53:11 -0300

    implementa 2.cpp

--- aula1/2.cpp
+++ aula1/2.cpp
@@ -11,12 +11,47 @@ gcc -pthread ...
 #include <string>
 #include <pthread.h>
 
+#define NUM_THREADS 2
+
 int number = -1;
+bool numberHasChanged = false;
 const char* filename;
 
+void* manipulate_file(void*);
+void* wait_for_number(void*);
 
 int main(int argc, char * argv[])
 {
 	filename = argv[1];
+
+    pthread_t threads[NUM_THREADS];
+    pthread_create(&threads[0], NULL, manipulate_file, NULL);
+    pthread_create(&threads[1], NULL, wait_for_number, NULL);
+
+    for (int i = 0; i < NUM_THREADS; i++)
+        pthread_join(threads[i], NULL);
+    
     return 0;
+}
+
+void* manipulate_file(void*) {
+    std::ifstream file;
+    file.open(filename);
+
+    if(!file.is_open())
+        exit(EXIT_FAILURE);
+    
+    file >> number;
+    file.close();
+    numberHasChanged = true;
+    printf("Thread B: %d\n", number);
+}
+
+void* wait_for_number(void*) {
+    while(1) {
+        if(numberHasChanged) {
+            printf("Thread A: %d\n", number);
+            break;
+        }
+    }
 }
