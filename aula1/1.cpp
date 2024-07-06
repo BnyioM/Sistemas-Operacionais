3a44b6b3dc252ebc2f17cf33fa71d6be615e9f7c
BnyioM <brunomaia.duarte@gmail.com>
2024-07-05 23:52:17 -0300

    aula1/1

--- aula1/1.cpp
+++ aula1/1.cpp
@@ -1,29 +1,50 @@
+/*
+gcc -pthread ... 
+*/
+
 #include <stdio.h>
+#include <sys/mman.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 
-void fork_exemplo(char * argv[]);
+void* manipulate_file(const char* filename, int* number);
 
-int main(int argc, char * argv[]) {
-    fork_exemplo(argv);
-    return 0;
-}
-
-void fork_exemplo(char * argv[]) {
+int main(int argc, char * argv[])
+{
     int id;
-    int32_t A;
-    FILE* arq;
+    int var;
+    int* number = (int*) mmap(NULL, sizeof (int) , PROT_READ | PROT_WRITE,
+                MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    *number = -1;
+    const char* filename = argv[1];
 
     id = fork();
 
     if (id == 0) {// Processo filho devido ao retorno zero
-        arq = fopen(argv[0], "r"); 
-        A = fscanf(arq, "%d", &A);
-        fclose(arq);
-        printf("Fork Filho: %d\n", A);
+        manipulate_file(filename, number);
     }
     // Processo pai porque o retorno é diferente de zero
     else if (id > 0) {
-        printf("Fork Pai: %d\n", A);
+        wait(NULL);
+        printf("Processo Pai: %d\n", *number);
     }
+    
+    return 0;
+}
+
+void* manipulate_file(const char* filename, int* number) {
+    std::ifstream file;
+    file.open(filename);
+
+    if (!file.is_open())
+        exit(EXIT_FAILURE);
+    
+    file >> *number;
+    file.close();
+    printf("Processo Filho: %d\n", *number);
 }
