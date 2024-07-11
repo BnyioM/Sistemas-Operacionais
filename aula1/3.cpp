f2a449ad2e384114883dca188589c7a5dd5a73de
BnyioM <brunomaia.duarte@gmail.com>
2024-07-11 19:44:11 -0300

    3

--- aula1/3.cpp
+++ aula1/3.cpp
@@ -7,6 +7,7 @@ gcc -pthread ...
 #include <pthread.h>
 #include <unistd.h>
 #include <math.h>
+#include <time.h>
 #include <iostream>
 
 void* worker(void*);
@@ -28,10 +29,14 @@ int main(int argc, char * argv[]){
 	pthread_t threads[n_threads];
 
 	for(int t = 0; t < n_threads; t++ )
-		pthread_create(&threads[t], NULL, worker, (void *)t);
+		pthread_create(&threads[t], NULL, worker, NULL);
+
+	int time_begin = clock();
 
 	for( int i = 0; i < n_threads; i++ )
 		pthread_join(threads[i], NULL);
+
+	printf("Tempo decorrido de execução das threads: %f", ((double) (clock() - time_begin))/CLOCKS_PER_SEC);
 
 	return 0;
 }
