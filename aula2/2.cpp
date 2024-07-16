95bd433557038f489bda1859c07047619fe577c8
GuiCastelo <guilherme.castelo@usp.br>
2024-07-16 15:51:09 -0300

    inicia implementacao do 2.cpp da aula2

--- aula2/2.cpp
+++ aula2/2.cpp
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include <semaphore.h>
 
 int acc = 0;
 int iteracoes = 10000;
@@ -14,10 +15,28 @@ int iteracoes = 10000;
 int main(int argc, char **argv) {
     int n = atoi(argv[1]);
 	pid_t pid = 1;
+
+    int* acc = (int*) mmap(NULL, sizeof (int) , PROT_READ | PROT_WRITE,
+                MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    sem_t* semaphore = (sem_t*) mmap(NULL, sizeof (sem_t) , PROT_READ | PROT_WRITE,
+                MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    sem_init(semaphore, 1, 1);
+    
 	/*Exemplo de como criar n filhos*/
-    for(int i = 0; i < n; i++) 
+    for(int i = 0; i < n - 1; i++) 
         if (pid != 0) 
             pid = fork();
+
+	int j = 0;
+	for (int i=0; i<iteracoes; i++){
+		j ++;
+	}
+	sem_wait(semaphore);
+	*acc = *acc + j;
+	sem_post(semaphore);
+    if(pid == 0)
+        exit(EXIT_SUCCESS);
 
 	/*NÃO ALTERE ESSE PRINTF*/
 	printf("Acc Esperado [%d] Acc obtido [%d]\n", n*iteracoes, acc);
