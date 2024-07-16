c712742fc1a12e4c58481326190684ecfcbbe1ca
BnyioM <brunomaia.duarte@gmail.com>
2024-07-16 16:26:42 -0300

    aula2/2 v1

--- aula2/2.cpp
+++ aula2/2.cpp
@@ -8,18 +8,39 @@
 #include <pthread.h>
 #include <stdbool.h>
 
-int acc = 0;
+int acc = 0; 
+int* number = (int*) mmap(NULL, sizeof (int) , PROT_READ | PROT_WRITE,
+                MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 int iteracoes = 10000;
+pthread_mutex_t* mutex = (pthread_mutex_t*) mmap(NULL, sizeof (pthread_mutex_t) , PROT_READ | PROT_WRITE,
+                MAP_SHARED | MAP_ANONYMOUS, -1, 0);;
 
 int main(int argc, char **argv) {
     int n = atoi(argv[1]);
+    *number = 0;
 	pid_t pid = 1;
 	/*Exemplo de como criar n filhos*/
     for(int i = 0; i < n; i++) 
         if (pid != 0) 
             pid = fork();
 
-	/*NÃO ALTERE ESSE PRINTF*/
-	printf("Acc Esperado [%d] Acc obtido [%d]\n", n*iteracoes, acc);
+    if (pid == 0) {
+        int j = 0;
+        for (int i=0; i<iteracoes; i++){
+            j ++;
+        }
+        pthread_mutex_lock(mutex);
+        *number = *number + j;
+        pthread_mutex_unlock(mutex);
+    } else if (pid > 0) {
+
+        for (int i = 0; i < n; i++) {
+            wait(NULL); // Aguarda todos os processos-filho
+        }
+
+        acc = *number;
+        /*NÃO ALTERE ESSE PRINTF*/
+        printf("Acc Esperado [%d] Acc obtido [%d]\n", n*iteracoes, acc);
+    }
     return 0;
 }
